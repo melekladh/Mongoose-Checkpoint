5327941550f4fb719645f31346b1b40c01d60020
melekladh <ladharimalek10@gmail.com>
2024-03-28 18:13:09 +0100

    second commit

--- models/userSchema.js
+++ models/userSchema.js
@@ -2,8 +2,7 @@ const mongoose =require("mongoose")
 const userSchema= mongoose.Schema({
     name: String ,
 age: Number,
-
-favoriteFoods: Object
+favoriteFoods: [String]
 })
 const User=mongoose.model("user",userSchema)
 module.exports=User
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const router=express.Router()
 const User=require("../models/userSchema")
-router.post("/post",async(req,res)=>{
+router.post("/postt",async(req,res)=>{
 
     try {
         const user=await User.create(req.body)
@@ -12,4 +12,69 @@ router.post("/post",async(req,res)=>{
     }
 })
 
+router.get("/get",async(req,res)=>{
+
+    try {
+        res.status(201).json({ msg:"user created"})
+    } catch (error) {
+        res.json({ err:"sthng went wrong try again ",error})
+
+    }
+})
+router.get("/getusers",async(req,res)=>{
+
+    try {
+        const users=await User.find()
+        res.status(200).json({msg:"get all users" ,users})
+
+    } catch (error) {
+        res.status(500).json({msg:"something went wrong" })
+
+    }
+})
+router.get("/getfoods/:food",async(req,res)=>{
+
+    try {
+        const users=await User.findOne({favoriteFoods:req.params.food})
+        res.status(200).json({msg:"get all users" ,users})
+
+    } catch (error) {
+        res.status(500).json({msg:"something went wrong" })
+
+    }
+})
+router.get("/getuser/:user",async(req,res)=>{
+
+    try {
+        const users=await User.findById(req.params.user)
+        res.status(200).json({msg:"get all users" ,users})
+
+    } catch (error) {
+        res.status(500).json({msg:"something went wrong" })
+
+    }
+})
+router.put("/updateuser/:user",async(req,res)=>{
+
+    try {
+        const users=await User.findByIdAndUpdate(req.params.user, req.body,{new:true})
+        res.status(200).json({msg:"get all users" ,users})
+
+    } catch (error) {
+        res.status(500).json({msg:"something went wrong" })
+
+    }
+})
+
+router.delete("/deleteuser/:user",async(req,res)=>{
+
+    try {
+        const users=await User.findByIdAndDelete(req.params.user, req.body)
+        res.status(200).json({msg:"get all users" ,users})
+
+    } catch (error) {
+        res.status(500).json({msg:"something went wrong" })
+
+    }
+})
 module.exports=router
